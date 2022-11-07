17ed034c75e635d51b33ebd48d714eb81b6e74cb
naxiwaynez10 <ibrahimnasiru17@gmail.com>
2022-11-07 14:54:30 +0100

    First commit

--- hooks.js
+++ hooks.js
@@ -3,21 +3,21 @@
 const eejs = require('ep_etherpad-lite/node/eejs');
 
 exports.eejsBlock_editbarMenuLeft = (hookName, args, cb) => {
-  args.content += eejs.require('ep_embedmedia/templates/editbarButtons.ejs', {}, module);
+  args.content += eejs.require('ep_embedmaths/templates/editbarButtons.ejs', {}, module);
   cb();
 };
 
 exports.eejsBlock_body = (hookName, args, cb) => {
-  args.content += eejs.require('ep_embedmedia/templates/modals.ejs', {}, module);
+  args.content += eejs.require('ep_embedmaths/templates/modals.ejs', {}, module);
   cb();
 };
 
 exports.eejsBlock_scripts = (hookName, args, cb) => {
-  args.content += eejs.require('ep_embedmedia/templates/scripts.ejs', {}, module);
+  args.content += eejs.require('ep_embedmaths/templates/scripts.ejs', {}, module);
   cb();
 };
 
 exports.eejsBlock_styles = (hookName, args, cb) => {
-  args.content += eejs.require('ep_embedmedia/templates/styles.ejs', {}, module);
+  args.content += eejs.require('ep_embedmaths/templates/styles.ejs', {}, module);
   cb();
 };
